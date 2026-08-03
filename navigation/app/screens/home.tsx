6e5d52c5437afc04c740a7f8878fd33e3abdeb15
amrit <vishal179singh@gmail.com>
2026-08-03 20:22:05 +0530

    navigation using useNavigation hook

--- navigation/app/screens/home.tsx
+++ navigation/app/screens/home.tsx
@@ -1,15 +1,17 @@
 import { StyleSheet, Text, View, Pressable } from 'react-native'
 import { Link } from "@react-navigation/native"
 import React from 'react'
+import {useNavigation} from "@react-navigation/native"
 
-const home = ({ navigation }: any) => {
+const home = () => {
+    const navigation = useNavigation()
     return (
         <View> 
             <Text>home</Text>
             {/* <Link
        screen={"Details"}> go to Details</Link> */}
             <Pressable onPress={() => navigation.navigate("Details")}>
-                <text>go to details</text>
+                <Text>go to details</Text>
             </Pressable>
         </View>
     )
